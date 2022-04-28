7374f0592c55b363ec97ad258a73803987b21479
SabariGiri0044 <97502707+SabariGiri0044@users.noreply.github.com>
2022-04-28 16:37:01 +0530

    Add files via upload

---
+++ SpringBootCrudOperations/src/main/java/com/Sabu/demo/SpringBootCrudOperationsApplication.java
@@ -0,0 +1,13 @@
+package com.Sabu.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudOperationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudOperationsApplication.class, args);
+	}
+
+}
---
+++ SpringBootCrudOperations/src/main/java/com/Sabu/demo/Student.java
@@ -0,0 +1,26 @@
+package com.Sabu.demo;
+
+public class Student {
+private int sid;
+private String sname;
+private String sdept;
+public int getSid() {
+	return sid;
+}
+public void setSid(int sid) {
+	this.sid = sid;
+}
+public String getSname() {
+	return sname;
+}
+public void setSname(String sname) {
+	this.sname = sname;
+}
+public String getSdept() {
+	return sdept;
+}
+public void setSdept(String sdept) {
+	this.sdept = sdept;
+}
+
+}
